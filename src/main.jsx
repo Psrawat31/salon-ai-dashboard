1828670fa83f19c16ed07a9d80e68ccacab13982
Psrawat31 <gr126101@gmail.com>
2025-11-25 23:13:09 +0530

    Fix BrowserRouter - Vercel map() error

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import { ThemeProvider } from "./ThemeContext.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </Router>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
